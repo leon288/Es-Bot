34cd132f95b44ba770e02bff070916e38354b29d
leon288 <68712863+leon288@users.noreply.github.com>
2020-08-06 16:06:14 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('Der Bot ist online!');
+});
+
+client.login('NzM3NDM1Nzg3NzMzMjM3ODMw.Xx9UmA.gBnMvajfIbAop-2UAdV_yQ9AkcQ');
